5b2e5f6d0f9b4558842eb4eb77e1c0989cb90917
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2025-12-02 17:50:29 +0100

    Removing unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { HashRouter } from 'react-router-dom';
@@ -8,7 +8,9 @@ import { GlobalStyle } from './GlobalStyle';
 import { theme } from './theme';
 import store from './store';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
@@ -18,6 +20,5 @@ ReactDOM.render(
         </HashRouter>
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

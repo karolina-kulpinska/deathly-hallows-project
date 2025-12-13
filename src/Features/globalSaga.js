a91e947f2bd8c3681a6eccd32357f993e495f0dd
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2025-12-13 18:23:40 +0100

    Implement API logic for search feature

--- src/Features/globalSaga.js
+++ src/Features/globalSaga.js
@@ -1,8 +1,34 @@
-import { all, call, debounce } from "redux-saga/effects";
-import { setSearchQuery } from "./globalSlice";
+import { all, call, debounce, put } from "redux-saga/effects";
+import { setSearchQuery, setMoviesData, setError, setLoading } from "./globalSlice";
+import tmdbApi from "../Api/tmdbApi";
 
-function* fetchMoviesHandler() {
-    console.log("-> OK, minęło 0.5 sekundy, dzwonię do TMDB...");
+function* fetchMoviesHandler(action) {
+    const query = action.payload;
+
+    if (query === "") {
+        yield put(setMoviesData(null));
+        return;
+    }
+
+    try {
+        yield put(setLoading(true));
+        yield put(setError(false));
+
+        const response = yield call(tmdbApi.get, "search/movie", {
+            params: {
+                query: query,
+                language: "pl-PL",
+                page: 1,
+            },
+        });
+
+        yield put(setMoviesData(response.data.results));
+
+    } catch (error) {
+        yield put(setError(true));
+        console.error("Błąd pobierania filmów:", error);
+
+    }
 }
 
 function* searchWatcher() {
--- src/Features/globalSlice.js
+++ src/Features/globalSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     searchQuery: "",
+    moviesData: null,
     isLoading: false,
     isError: false,
 };
@@ -12,16 +13,20 @@ const globalSlice = createSlice({
     reducers: {
         setSearchQuery: (state, { payload }) => {
             state.searchQuery = payload;
-            state.isLoading = true;
+        },
+        setMoviesData: (state, { payload }) => {
+            state.moviesData = payload;
+            state.isLoading = false;
         },
         setLoading: (state, { payload }) => {
             state.isLoading = payload;
         },
         setError: (state, { payload }) => {
             state.isError = payload;
+            state.isLoading = false;
         }
     },
 });
 
-export const { setSearchQuery, setLoading, setError } = globalSlice.actions;
+export const { setSearchQuery, setMoviesData, setLoading, setError } = globalSlice.actions;
 export default globalSlice.reducer;

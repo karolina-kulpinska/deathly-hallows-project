d9286509028a74ff076d2caee3d2452b07ea4ca8
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2025-12-13 19:26:07 +0100

    Changing TMDB authorization from API Key V3 to Access Token

--- src/Api/tmdbApi.js
+++ src/Api/tmdbApi.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'https://api.themoviedb.org/3';
-const API_KEY = process.env.REACT_APP_API_KEY;
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN_V4;
 
 const tmdbApi = axios.create({
     baseURL: API_URL,
-    // Kluczowy element: dodaje api_key do każdego zapytania! Pamiętajcie o tym!
-    params: {
-        api_key: API_KEY,
+    headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
     },
+
 });
 
 export default tmdbApi;

4cada19ba1194b030c3377e70aae7f4d4800e686
TwojaNazwaGitHub <twoj-email@gmail.com>
2025-12-22 17:39:52 +0100

    poprawiony NoResultsView

--- src/common/NoResultsView/index.js
+++ src/common/NoResultsView/index.js
@@ -1,11 +1,10 @@
 import { Container, Header, NoResultsImage } from "./styled";
 
-
-const NoResultsView = () => {
+const NoResultsView = ({ query }) => {
     return (
         <Container>
-            <Header>Sorry, there are no results for "Muan"</Header>
-            <NoResultsImage></NoResultsImage>
+            <Header>Sorry, there are no results for "{query}"</Header>
+            <NoResultsImage />
         </Container>
     );
 };

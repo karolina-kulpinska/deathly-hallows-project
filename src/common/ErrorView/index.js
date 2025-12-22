ae4b463ee848e224d1b28ea4378b654801519b2d
TwojaNazwaGitHub <twoj-email@gmail.com>
2025-12-22 22:01:34 +0100

    feat: add Danger icon import as requested by Karolina

--- src/common/ErrorView/index.js
+++ src/common/ErrorView/index.js
@@ -1,10 +1,13 @@
+import { ReactComponent as Danger } from "./Danger.svg";
 import { Container, DangerIcon, Header, HomepageButton, Info } from "./styled";
 
 const ErrorView = () => {
 
     return (
         <Container>
-            <DangerIcon></DangerIcon>
+            <DangerIcon>
+                <Danger />
+            </DangerIcon>
             <Header>Ooops! Something went wrong...</Header>
             <Info>Please check your network connection<br/>and try again</Info>
             <HomepageButton>Back to home page</HomepageButton>

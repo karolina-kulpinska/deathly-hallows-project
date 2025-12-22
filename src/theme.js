372f0f3196b12762d98adccf5bccb3d22f52291f
TwojaNazwaGitHub <twoj-email@gmail.com>
2025-12-22 20:46:39 +0100

    feat: add missing breakpoints to global theme

--- src/theme.js
+++ src/theme.js
@@ -2,9 +2,12 @@ export const theme = {
     color: {
         black: "rgba(0, 0, 0, 1)",
         white: "rgba(255, 255, 255, 1)",
-
+        woodsmoke: "#18181B",
     },
     breakpoints: {
-        mobileMax: "767px",
+        mobileMax: 767,
+        tabletVerticalMax: 991,
+        tabletHorizontalMax: 1199,
+        desktopMax: 1399,
     },
 };

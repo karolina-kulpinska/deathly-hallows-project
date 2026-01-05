cdb674c20e67de9b1c0bcde094e06a70b471a4a4
TwojaNazwaGitHub <twoj-email@gmail.com>
2026-01-05 20:30:31 +0100

    style: refactor MovieDetails to use theme variables and fix template literals

--- src/Features/MovieDetails/styled.js
+++ src/Features/MovieDetails/styled.js
@@ -1,26 +1,25 @@
-
 import styled from "styled-components";
 
 export const Page = styled.main`
   max-width: 1368px;
-  margin: 56px auto 103px; // Zmiana marginesów na zgodne z makietą
+  margin: 56px auto 103px;
   padding: 0 16px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints?.mobileMax || "767px"}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     margin: 24px auto 40px;
   }
 `;
 
 export const Title = styled.h2`
-  margin: 0 0 24px; // Odstęp pod nagłówkiem
+  margin: 0 0 24px;
   font-family: 'Poppins', sans-serif;
   font-style: normal;
   font-weight: 600;
   font-size: 36px;
   line-height: 1.2;
-  color: ${({ theme }) => theme.text || "#18181B"};
+  color: ${({ theme }) => theme.color.woodsmoke};
 
-  @media (max-width: ${({ theme }) => theme.breakpoints?.mobileMax || "767px"}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     font-size: 18px;
     margin-bottom: 12px;
   }

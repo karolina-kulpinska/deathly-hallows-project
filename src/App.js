537276aa96cad2b447fe6c268f588d935acb9359
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2026-01-06 00:14:30 +0100

    add movie details API calls and route configuration

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { MovieList } from "./Features/MovieList";
 import { PersonList } from "./Features/PersonList";
 import Navigation from "./Features/Navigation";
 import { PersonDetails } from "./Features/PersonDetails";
+import MovieDetails from "./Features/MovieDetails";
+
 const App = () => (
   <HashRouter>
     <Navigation />
 
     <Switch>
+      <Route path="/movie/:id">
+        <MovieDetails />
+      </Route>
       <Route path="/person/:id">
         <PersonDetails />
       </Route>

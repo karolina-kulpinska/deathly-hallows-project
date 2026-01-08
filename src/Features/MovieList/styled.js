cdf0df7dadf40a0049b92a0003d3eb866f7b23d0
sukiennikk <sukiennikk@icloud.com>
2026-01-08 22:07:45 +0100

    media breakpoints changed to previous

--- src/Features/MovieList/styled.js
+++ src/Features/MovieList/styled.js
@@ -15,14 +15,6 @@ export const MoviesGrid = styled.div`
   grid-template-columns: repeat(4, 1fr);
   gap: 24px;
 
-  @media (max-width: 1200px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  @media (max-width: 900px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     grid-template-columns: 1fr;
     gap: 16px;
